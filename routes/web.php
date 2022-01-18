7ac7a57ac10f95af3193dd8ff5924d040a4d5a99
tijjanidev <tijjanidev@gmail.com>
2022-01-18 16:43:56 +0000

    msge

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PermissionController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
+    Route::resource('clients', App\Http\Controllers\ClientsController::class)->except('edit', 'update', 'destroy');
 
-Route::resource('clients', App\Http\Controllers\ClientsController::class)->except('edit', 'update', 'destroy');
+    Route::resource('projects', App\Http\Controllers\ProjectsController::class)->except('edit', 'update', 'destroy');
+    
+    Route::resource('issues', App\Http\Controllers\IssuesController::class)->except('edit', 'update', 'destroy');
 
-Route::resource('projects', App\Http\Controllers\ProjectsController::class)->except('edit', 'update', 'destroy');
+    
+    // Route::prefix('/users')->group(function(){
+    //     Route::get('/{id}/activate', [App\Http\Controllers\UsersController::class, 'activate']);
+    //     Route::get('/{id}/deactivate', [App\Http\Controllers\UsersController::class, 'deactivate']);
+    // });
 
-Route::resource('issues', App\Http\Controllers\IssuesController::class)->except('edit', 'update', 'destroy');
+    Route::resource('users', App\Http\Controllers\UserController::class);
+});

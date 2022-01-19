becfa36c79819a7da0a796409b07197fbf149041
tijjanidev <tijjanidev@gmail.com>
2022-01-19 16:47:28 +0000

    msge

--- app/Http/Livewire/Projects/Index.php
+++ app/Http/Livewire/Projects/Index.php
@@ -6,8 +6,18 @@ use Livewire\Component;
 
 class Index extends Component
 {
+
+    protected $projects;
+
+    public function mount($projects){
+        $this->projects = $projects;
+    }
+
     public function render()
     {
-        return view('livewire.projects.index');
+        $projects = $this->projects;
+        return view('livewire.projects.index',[
+            'projects' => $projects
+        ]);
     }
 }

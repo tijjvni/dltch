7ef26687f26170265c695d3e3be680d20372e422
tijjanidev <tijjanidev@gmail.com>
2022-01-19 12:26:30 +0000

    msge

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -34,8 +34,4 @@ class Client extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -61,4 +61,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function client()
+    {
+        return $this->hasOne(Client::class);
+    }
 }

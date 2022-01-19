c43c718bc7e6ab0935864bf4785b1fef3b5b0a2c
tijjanidev <tijjanidev@gmail.com>
2022-01-19 16:58:07 +0000

    msge

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,11 +3,11 @@
         <div class="flex w-full">
             <div class="flex-grow">
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    {{ __('Clients') }}
+                    {{ __('Projects') }}
                 </h2>
             </div>
             <div class="flex-initial">
-                <a  href="{{ route('clients.create') }}" >
+                <a  href="{{ route('projects.create') }}" >
                     <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Create
                     </button>
@@ -18,6 +18,6 @@
 
 
     <div class="p-5">
-        @livewire('clients.create')
+        @livewire('projects.create')
     </div>
 </x-app-layout>

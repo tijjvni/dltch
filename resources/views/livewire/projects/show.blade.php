adb5ddcb617a1ba39c12edda507ce0a259f00114
tijjanidev <tijjanidev@gmail.com>
2022-01-30 14:42:22 +0000

    msge

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -1,3 +1,21 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <div class="relative text-center">
+        <span class="absolute inset-x-0 h-px -translate-y-1/2 bg-black/10 top-1/2"></span>
+        
+        <h2 class="relative inline-block px-4 text-2xl font-bold text-center bg-white">
+            {{ $project->title }}
+        </h2>
+    </div>
+      
+    <section>
+        <strong class="inline-block px-3 py-1 text-xs font-semibold text-green-600 bg-green-100 rounded-sm">
+          On going
+        </strong>
+      
+        <h2 class="mt-2 text-lg font-bold">
+            {{ $project->description }}
+        </h2>
+    </section>
+      
+
 </div>

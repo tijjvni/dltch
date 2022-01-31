1ad26cf498a195d6800bdd639d06178bbac184ff
tijjanidev <tijjanidev@gmail.com>
2022-01-31 12:21:14 +0000

    msge

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -18,7 +18,21 @@
 
 	<!-- Component Start -->
 	<div class="grid md:grid-cols-2 gap-2 w-full p-5">
+
 		
+		<!-- Tile 2 -->
+		<div class="flex items-center p-4 bg-white rounded">
+			<div class="flex flex-shrink-0 items-center justify-center bg-indigo-200 h-16 w-16 rounded">
+				<i class="fa fa-folder-open text-indigo-600"></i>
+			</div>
+			<div class="flex-grow flex flex-col ml-4">
+				<span class="text-xl font-bold">{{ $projectsCount }}</span>
+				<div class="flex items-center justify-between">
+					<span class="text-gray-500">All projects</span>
+				</div>
+			</div>
+		</div>
+
 		<!-- Tile 1 -->
 		<div class="flex items-center p-4 bg-white rounded">
 			<div class="flex flex-shrink-0 items-center justify-center bg-indigo-200 h-16 w-16 rounded">
@@ -32,19 +46,7 @@
 				</div>
 			</div>
 		</div>
-		
-		<!-- Tile 2 -->
-		<div class="flex items-center p-4 bg-white rounded">
-			<div class="flex flex-shrink-0 items-center justify-center bg-indigo-200 h-16 w-16 rounded">
-				<i class="fa fa-tasks text-indigo-600"></i>
-			</div>
-			<div class="flex-grow flex flex-col ml-4">
-				<span class="text-xl font-bold">{{ $projectsCount }}</span>
-				<div class="flex items-center justify-between">
-					<span class="text-gray-500">All projects</span>
-				</div>
-			</div>
-		</div>
+
 		
 	</div>
 	<!-- Component End  -->

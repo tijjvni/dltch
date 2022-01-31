1925ab7dc9cab4bfb6ff170c247a12aa119a51a7
tijjanidev <tijjanidev@gmail.com>
2022-01-31 11:48:53 +0000

    msge

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -39,7 +39,7 @@
             <button class="mx-2 my-2 bg-white border transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white hover:bg-gray-100 rounded text-indigo-700 px-6 py-2 text-lg"> + </button>
             </div> 
         </div>
-        <div class="px-4 py-5 sm:px-6 flex w-full border dark:bg-gray-800 bg-white shadow mb-2 rounded-md">
+        <div class="px-4 py-5 sm:px-6 flex flex-col w-full border dark:bg-gray-800 bg-white shadow mb-2 rounded-md">
 
             <div class="py-2 flex flex-row items-center justify-between">
                 <div class="flex flex-row items-center">

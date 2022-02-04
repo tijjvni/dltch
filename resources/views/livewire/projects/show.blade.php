c62ce94b7724e25f4da5946dd1f84f770ce7dea1
tijjanidev <tijjanidev@gmail.com>
2022-02-04 11:32:18 +0000

    msge

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -38,20 +38,36 @@
                 </p>
             </div>
             <div class="flex-initial justify-end">
-                <button type="button"
-                    x-data="{}" x-on:click="window.livewire.emitTo('add-issue-modal', 'show')"
-                    class="inline-flex content-end px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-white" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd"
-                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
-                    </span>Click to Open Modal
-                </button>
-                @livewire('add-issue-modal')
+                <div x-data="{ open: false }">
+                    <button x-ref="modal1_button"
+                            @click="open = true"
+                            class="w-full bg-indigo-600 px-4 py-2 border border-transparent rounded-md flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:inline-flex">
+                            Open Modal
+                    </button>
                 
+                    <div role="dialog"
+                         aria-labelledby="modal1_label"
+                         aria-modal="true"
+                         tabindex="0"
+                         x-show="open"
+                         @click="open = false; $refs.modal1_button.focus()"
+                         @click.away="open = false; $refs.modal1_button.focus()"
+                         class="fixed top-0 left-0 w-full h-screen flex justify-center items-center">
+                        <div class="absolute top-0 left-0 w-full h-screen bg-black opacity-60"
+                             aria-hidden="true"
+                             x-show="open"></div>
+                        <div @click.stop=""
+                             x-show="open"
+                             class="flex flex-col rounded-lg shadow-lg overflow-hidden bg-white w-3/5 h-3/5 z-10">
+                          <div class="p-6 border-b">
+                            <h2 id="modal1_label">Header</h2>
+                          </div>
+                          <div class="p-6">
+                            Content
+                          </div>
+                        </div>
+                    </div>
+                </div>                
             </div> 
         </div>
         <div class="px-4 py-5 sm:px-6 flex flex-col divide-y divide-gray-500 w-full border dark:bg-gray-800 bg-white shadow mb-2 rounded-md">

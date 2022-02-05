5e4d4bee94f3c2a120cb08995329145320a28066
tijjvni <tijjvni@gmail.com>
2022-02-05 09:37:22 +0000

    msg

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -39,42 +39,40 @@
             </div>
             <div class="flex-initial justify-end">
            
-                <div
-                x-data="{ 'showModal': false }"
-                @keydown.escape="showModal = false"
-                >
-                    <!-- Trigger for Modal -->
-                    <button type="button" @click="showModal = true">Open Modal</button>
-
-                    <!-- Modal -->
-                    <div
-                        class="fixed inset-0 z-30 flex items-center justify-center overflow-auto bg-black bg-opacity-50"
-                        x-show="showModal"
-                    >
-                        <!-- Modal inner -->
-                        <div
-                            class="max-w-3xl px-6 py-4 mx-auto text-left bg-white rounded shadow-lg"
-                            @click.away="showModal = false"
-                            x-transition:enter="motion-safe:ease-out duration-300"
-                            x-transition:enter-start="opacity-0 scale-90"
-                            x-transition:enter-end="opacity-100 scale-100"
-                        >
-                            <!-- Title / Close-->
-                            <div class="flex items-center justify-between">
-                                <h5 class="mr-3 text-black max-w-none">Title</h5>
-
-                                <button type="button" class="z-50 cursor-pointer" @click="showModal = false">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
-                                </button>
-                            </div>
-
-                            <!-- content -->
-                            <div>Content goes here</div>
+        <!-- modal div -->
+        <div class="mt-6" x-data="{ open: false }">
+            <!-- Button (blue), duh! -->
+            <button class="px-4 py-2 text-white bg-blue-500 rounded select-none no-outline focus:shadow-outline" @click="open = true">Open Modal</button>
+            <!-- Dialog (full screen) -->
+            <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full" style="background-color: rgba(0,0,0,.5);" x-show="open"  >
+                <!-- A basic modal dialog with title, body and one button to close -->
+                <div class="h-auto p-4 mx-2 text-left bg-white rounded shadow-xl md:max-w-xl md:p-6 lg:p-8 md:mx-0" @click.away="open = false">
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <h3 class="text-lg font-medium leading-6 text-gray-900">
+                        Modal Title Here
+                        </h3>
+                        <div class="mt-2">
+                            <p class="text-sm leading-5 text-gray-500">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
                         </div>
                     </div>
+                    <!-- One big close button.  --->
+                    <div class="mt-5 sm:mt-6">
+                        <span class="flex w-full rounded-md shadow-sm">
+                            <button @click="open = false" class="inline-flex justify-center w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700">
+                            Close this modal!
+                            </button>
+                        </span>
+                    </div>
                 </div>
+            </div>
+        </div>
           
             </div> 
         </div>

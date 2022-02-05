6c396f12e7a9edcf91b65184c6d9d9f35b40ad79
tijjvni <tijjvni@gmail.com>
2022-02-05 09:53:01 +0000

    msg

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -39,8 +39,13 @@
             </div>
             <div class="flex-initial justify-end">
                 <div x-data="{show:false}" class="container flex justify-center mx-auto">
-                    <button @click="show=true" class="px-6 py-2 text-white bg-blue-600 rounded shadow-xl" type="button">open
-                        model</button>
+                    <button 
+                    type="button" 
+                    class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"  
+                    @click="show=true" >
+                        Add issue
+                    </button>
+                
                     <div x-show="show" class="absolute inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50">
                         <div @click.away="show = false" class="max-w-sm p-6 bg-white ">
                             <div class="flex items-center justify-between">
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -11,7 +11,7 @@
                     <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Create
                     </button>
-                </a>   
+                </a> 
             </div>
         </div>
     </x-slot>

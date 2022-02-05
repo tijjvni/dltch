1f2b86990273d0d12ce802e333f66dbeba0b8461
tijjvni <tijjvni@gmail.com>
2022-02-05 09:44:30 +0000

    msg

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -39,40 +39,16 @@
             </div>
             <div class="flex-initial justify-end">
            
-        <!-- modal div -->
-        <div class="mt-6" x-data="{ open: false }">
-            <!-- Button (blue), duh! -->
-            <button class="px-4 py-2 text-white bg-blue-500 rounded select-none no-outline focus:shadow-outline" @click="open = true">Open Modal</button>
-            <!-- Dialog (full screen) -->
-            <div class="absolute top-0 left-0 flex items-center justify-center w-full min-h-screen" style="background-color: rgba(0,0,0,.5);" x-show="open"  >
-                <!-- A basic modal dialog with title, body and one button to close -->
-                <div class="h-auto p-4 mx-2 text-left bg-white rounded shadow-xl md:max-w-xl md:p-6 lg:p-8 md:mx-0" @click.away="open = false">
-                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                        <h3 class="text-lg font-medium leading-6 text-gray-900">
-                        Modal Title Here
-                        </h3>
-                        <div class="mt-2">
-                            <p class="text-sm leading-5 text-gray-500">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
-                        </div>
-                    </div>
-                    <!-- One big close button.  --->
-                    <div class="mt-5 sm:mt-6">
-                        <span class="flex w-full rounded-md shadow-sm">
-                            <button @click="open = false" class="inline-flex justify-center w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700">
-                            Close this modal!
-                            </button>
-                        </span>
+            <div x-data="{modal: false}">
+                <button class="btn btn-primary mb-3 scroll-lock" @click="modal = !modal" :aria-expanded="modal ? 'true' : 'false'">Modal example</button>
+                <div class="modal-wrapper" :class="{ 'active': modal }">
+                    <div class="backdrop backdrop-shaded"></div>
+                    <div class="modal-panel m-3 p-3 b-thin rounded bg-color-background" :class="{ 'active': modal }">
+                    <p>Modal content.</p>
+                    <button class="float-right scroll-unlock" @click="modal = false">Close</button>
                     </div>
                 </div>
             </div>
-        </div>
           
             </div> 
         </div>

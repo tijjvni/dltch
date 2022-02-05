4ad20e6b728d9841c6d928e5f1610e2b290dd050
tijjvni <tijjvni@gmail.com>
2022-02-05 09:40:33 +0000

    msg

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -44,7 +44,7 @@
             <!-- Button (blue), duh! -->
             <button class="px-4 py-2 text-white bg-blue-500 rounded select-none no-outline focus:shadow-outline" @click="open = true">Open Modal</button>
             <!-- Dialog (full screen) -->
-            <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full" style="background-color: rgba(0,0,0,.5);" x-show="open"  >
+            <div class="absolute top-0 left-0 flex items-center justify-center w-full min-h-screen" style="background-color: rgba(0,0,0,.5);" x-show="open"  >
                 <!-- A basic modal dialog with title, body and one button to close -->
                 <div class="h-auto p-4 mx-2 text-left bg-white rounded shadow-xl md:max-w-xl md:p-6 lg:p-8 md:mx-0" @click.away="open = false">
                     <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">

5afa07ddede1b5fc39cfaa6ee42026cb151fe23b
tijjvni <tijjvni@gmail.com>
2022-02-06 14:13:32 +0000

    msg

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -38,7 +38,7 @@
                 </p>
             </div>
             <div class="flex-initial justify-end">
-                <div x-data="{ open: @entangle('showDropdown') }" class="container flex justify-center mx-auto">
+                <div x-data="{ open: @entangle('addIssue') }" class="container flex justify-center mx-auto">
                     <button 
                     type="button" 
                     class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"  

49789e1fe8805310affaaf83d39c70aa31e113c9
tijjvni <tijjvni@gmail.com>
2022-02-06 13:11:47 +0000

    msg

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -53,27 +53,28 @@
                         x-show="show" 
                     >
 
+                        <div @click.away="show=false" >
 
-                        <x-jet-dialog-modal @click.away="show = false">
-                            <x-slot name="title">
-                                Delete Account
-                            </x-slot>
-
-                            <x-slot name="content">
-                                Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted.
-                            </x-slot>
-
-                            <x-slot name="footer">
-                                <x-jet-secondary-button wire:click="$toggle('confirmingUserDeletion')" wire:loading.attr="disabled">
-                                    Nevermind
-                                </x-jet-secondary-button>
-
-                                <x-jet-danger-button class="ml-2" wire:click="deleteUser" wire:loading.attr="disabled">
+                            <x-jet-dialog-modal>
+                                <x-slot name="title">
                                     Delete Account
-                                </x-jet-danger-button>
-                            </x-slot>
-                        </x-jet-dialog-modal>
+                                </x-slot>
 
+                                <x-slot name="content">
+                                    Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted.
+                                </x-slot>
+
+                                <x-slot name="footer">
+                                    <x-jet-secondary-button wire:click="$toggle('confirmingUserDeletion')" wire:loading.attr="disabled">
+                                        Nevermind
+                                    </x-jet-secondary-button>
+
+                                    <x-jet-danger-button class="ml-2" wire:click="deleteUser" wire:loading.attr="disabled">
+                                        Delete Account
+                                    </x-jet-danger-button>
+                                </x-slot>
+                            </x-jet-dialog-modal>
+                        </div>
                     </div>
                 </div>
 

5808b9ca3f6fd1bf73cc5d495dd9fce264a431d4
tijjvni <tijjvni@gmail.com>
2022-02-08 11:05:47 +0000

    mmssge

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -8,11 +8,11 @@
 
     <div x-show="open" class="fixed inset-0 overflow-y-auto px-4 py-6 md:py-24 sm:px-0 z-40">
         <div class="fixed inset-0 transform">
-            <div class="absolute inset-0 bg-gray-500 opacity-75">
-
-            </div>
+            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
-        <div @click.away="open = false"  class="bg-white rounded-lg overflow-hidden transform sm:w-full sm:mx-auto max-w-lg">      
+        <div 
+        @click.away="open = false"  
+        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto">
             {{ $slot }}
         </div>
     </div>

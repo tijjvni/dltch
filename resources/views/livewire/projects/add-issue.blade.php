4945894f81c371b94d728a371fdfa573de109ef3
tijjvni <tijjvni@gmail.com>
2022-02-08 09:10:14 +0000

    msg

--- resources/views/livewire/projects/add-issue.blade.php
+++ resources/views/livewire/projects/add-issue.blade.php
@@ -1,4 +1,9 @@
-<div>
+<div class="fixed inset-0 overflow-y-auto px-4 py-6 md:py-24 sm:px-0 z-40">
+    <div class="fixed inset-0 transform">
+        <div class="absolute inset-0 bg-gray-500 opacity-75">
+
+        </div>
+    </div>
     @if($show)
         <x-jet-form-section submit="createProject">
             <x-slot name="title">

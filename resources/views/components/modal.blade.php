fec1846655576218566bd21e52d0c269c44794af
tijjvni <tijjvni@gmail.com>
2022-02-08 22:33:37 +0000

    msg

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -27,9 +27,7 @@
                 </div>
             </div>
 
-                <!-- <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 text-right">
-                    {{ footer }}
-                </div> -->
+            
 
         </div>
     </div>
